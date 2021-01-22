ee617608694321a96fbbc8273d25b4f60266a0cc
johnaclufi <66035904+johnaclufi@users.noreply.github.com>
2021-01-21 23:24:22 -0500

    Update createClient.go

--- createClient.go
+++ createClient.go
@@ -2,23 +2,23 @@ package SecureClient
 
 /*
 	Create new SSL-pinned client
-	
+
 	- The dialerConfig contains all of the generated pins
 	- dialerConfig only needs to be made once, it should be shared and used on creation of new clients
 
 */
 
 import (
-	"fmt"
-	"strings"
 	"errors"
-	"net/url"
+	"fmt"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"strings"
 )
 
-
 //--- Wrapper for go's standard implementation of the http.RoundTripper interface
-type SSLPinnerTransport struct{
+type SSLPinnerTransport struct {
 	DefaultTransport *http.Transport
 	BadPinDetected   func(proxy string)
 	Proxy            string
@@ -27,23 +27,34 @@ type SSLPinnerTransport struct{
 //--- Implementing http.RoundTripper
 func (t *SSLPinnerTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 	resp, err := t.DefaultTransport.RoundTrip(r)
-	
+
 	if err != nil {
 		if strings.Contains(fmt.Sprintf("%v", err), "x509: certificate signed by unknown authority") {
 			t.BadPinDetected(t.Proxy)
 			return resp, errors.New("Security Error - Unsafe proxy")
 		}
 	}
-	
+
 	return resp, err
 }
 
+//NewClient new SSL PINNER CLIENT
 func (p *SSLPinner) NewClient(proxy string) (*http.Client, error) {
-	client, defaultTransport := &http.Client{}, &http.Transport{}
 
-	if proxy != "" {	
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		// error handling
+	}
+
+	client, defaultTransport := &http.Client{
+		Jar: jar,
+	}, &http.Transport{}
+
+	if proxy != "" {
 		u, err := url.Parse(proxy)
-		if err != nil { return client, err }
+		if err != nil {
+			return client, err
+		}
 		defaultTransport = &http.Transport{
 			DialTLS: p.DialerConfig.NewDialer(),
 			Proxy:   http.ProxyURL(u),
@@ -54,20 +65,20 @@ func (p *SSLPinner) NewClient(proxy string) (*http.Client, error) {
 			//--- No proxy
 		}
 	}
-	
+
 	client.Transport = &SSLPinnerTransport{
 		//--- Standard go transport - If you have a custom transport, plug it in here
 		//---   uTLS, etc.
 		DefaultTransport: defaultTransport,
-		
+
 		//--- Triggered when SSL pin is not matched, ie mitm proxy passes an invalid cert
-		BadPinDetected:   p.BadPinDetected,
-		
+		BadPinDetected: p.BadPinDetected,
+
 		//--- Store proxy here for logging in BadPinDetected
 		//--- It would be easy to see if the proxy seems like a MITM proxy
-		//--- http://localhost:5555,  http://localhost:8080 etc.  
+		//--- http://localhost:5555,  http://localhost:8080 etc.
 		Proxy: proxy,
 	}
-	
+
 	return client, nil
 }
